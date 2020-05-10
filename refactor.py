a6d150ccd22960ec8f8c414aa87f1af2ec6e52d8
dorzac <dorzac0@gmail.com>
2020-05-10 16:27:42 -0500

    prehistoric issue, dox

--- refactor.py
+++ refactor.py
@@ -1,13 +1,9 @@
 """High Priority"""
-# TODO: 'Where a periodO def only has 1 date for start or end or both, 
-#		 leave latestStart and earliestEnd blank.'
 """Low Priority"""
 # TODO: Write custom sort for periodo terms that's safer than zipping
 # TODO: Use dates as a refinement mechanism
-# TODO: Instead of looking for TRI + TERM + DATE, look for TRI + TERM 
-#		and TRI + DATE separately
-# TODO: Make variables like multi_terms more general, have a method to 
-#		find these.
+# TODO: Instead of looking for TRI + TERM + date, look for TRI + TERM 
+#		and TRI + date separately
 
 import os
 import re
@@ -26,57 +22,32 @@ from record import *
 Useful Global Variables, Expressions, etc
 """
 
-#Sentence Splitting
-alphabets = "([A-Za-z])"
-prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
-suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-starters = ("(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|"
-			"We\s|But\s|However\s|That\s|This\s|Wherever)")
-acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-websites = "[.](com|net|org|io|gov)"
-
 #Constants
-timeExclude = ["late", "early", "the past", "once", "falls", \
-				"previously", "now", "fall", "recently", "time", \
-				"present", "past", "current", "currently"]
-countycodes = re.compile("\s([A-Z]{2})[\s,\.,\,]")
-trinomial_regex = re.compile("41[a-zA-Z]{2}[0-9]{1,4}")
-#bp_dates = re.compile("\d+ B\.?[P]\.?") #only BP
+TRINOMIAL_REGEX = re.compile("41[a-zA-Z]{2}[0-9]{1,4}")
 bp_dates = re.compile("\d+(\s?b\.?[p,c]\.?)?\s?(.|to|and)?\s?\d+\s?(b\.?[p,c]\.?)")
 pub_date = re.compile("((19|20)[0-9]{2})")		
-cents = re.compile("(((beginning|middle|end|early|mid|late|[0-9a-z]*(st|[^a]nd|rd|th)\s)[a-z ]*)?(([a-z]*(\-|\s))?[a-z]*)[0-9]{0,2}?(st|nd|rd|th)\s(century|millenia)(\s(b\.?c\.?e\.?|c\.?e\.?|b\.?c\.?|a\.?d\.?))?)")
-
-NEGATIVES = [" none ", " not ", " no "] #CONSIDER: 'yet to find'
+cents = re.compile("(((beginning|middle|end|early|mid|late|[0-9a-z]*(st|[^a]nd|rd|th)" +
+		"\s)[a-z ]*)?(([a-z]*(\-|\s))?[a-z]*)[0-9]{0,2}?(st|nd|rd|th)\s(century|millenia)" +
+		"(\s(b\.?c\.?e\.?|c\.?e\.?|b\.?c\.?|a\.?d\.?))?)")
+NEGATIVES = [" none ", " not ", " no "] 
+SEARCH_SIZE = 3 # size 3 seems to be about optimal
 
 #Globals
 human_readable = False
 input_file = ""
 date = 0
 relevant_trinomials = []
-set_of_vals = []
-counties = []
 periodo = []
 
-SEARCH_SIZE = 3 # size 3 seems to be about optimal
-
-#Set up s.t. dictionary can translate county codes into names
-cnames = []
-ccodes = []
-for item in counties:
-	cnames.append(item[0])
-	ccodes.append(item[1])
-cmap = dict(zip(ccodes, cnames))
-
-multi_terms = ['Archaic', 'Historic', 'Early Archaic',\
-		'Late Archaic', 'Late Paleoindian', 'Late Prehistoric', \
-		'Middle Archaic', 'Paleoindian',]
 
 def harvest():
 	"""
-	Method called first.
+	@return relevant trinomials, a list of trinomials to consider in the output
+	@return periodo, global matrix representation of the csv data
+	Method called first, all preprocessing work.
 	Reads in vocabulary files and stores them.
 	"""
-
+	
 	#get relevant trinomials from csv
 	relevant_trinomials = []
 	with open('vocabularies/relevantTrinomials.csv', newline='') as f:
@@ -85,11 +56,6 @@ def harvest():
 	for sublist in data:
 		if sublist[0] != '':
 			relevant_trinomials.append(sublist[0])
-
-	#get counties from csv
-	with open('vocabularies/relevantCounties.csv', newline='') as f:
-		reader = csv.reader(f)
-		counties = list(reader)
 
 	#get periodo vocabs from all periodo csvs
 	periodo = []
@@ -110,28 +76,20 @@ def harvest():
 					if data[row][1] is not '':
 						data[row][1] = data[row][1].split('Phase',1)[0]
 
-			#Transpose columns and rows of the CSV,
-			#ignoring the labels, for convenience
+			#Transpose columns and rows of the CSV, ignoring labels
 			for row in range(1, len(data)):
 				for column in range(len(data[row])):
-					#if data[row][column] is not '':
-					print(filename)
-					print(row,column)
 					periodo[column].append(data[row][column])
 
+	# Sort terms alphabetically
 	periodo[0], periodo[1], periodo[4], periodo[5], periodo[8], periodo[10], periodo[11] = \
 		(list(t) for t in zip(*sorted( \
 		zip(periodo[0], periodo[1], periodo[4], periodo[5], periodo[8], \
 		periodo[10], periodo[11]), \
 		key=lambda l1:l1[1])))
-	"""
-	print("NAGA START")
-	for e in range(len(periodo[1])):
-		print(periodo[1][e], periodo[4][e], periodo[5][e], periodo[8][e])
-	print("NAGA END")
-	"""
 
-	# TODO: This only sorts 5 columns in parallel. Maybe write
+	# Sort terms by descending string len
+	# TODO: This only sorts 7 columns in parallel. Maybe write
 	# custom sort to clean this up
 	periodo[0], periodo[1], periodo[4], periodo[5], periodo[8], \
 		periodo[10], periodo[11] = \
@@ -140,15 +98,28 @@ def harvest():
 		periodo[10], periodo[11]), \
 		key=lambda l1:len(l1[1]), reverse=True)))
 
-	"""
-	print("NAGA START")
-	for e in range(len(periodo[1])):
-		print(periodo[1][e], periodo[4][e], periodo[5][e], periodo[8][e])
-	print("NAGA END")
-	"""
+	return relevant_trinomials, periodo
 
 
-	return relevant_trinomials, counties, periodo
+def get_multi_terms(terms):
+	"""
+	@param terms is a list of terms to find multiple instances of in the periodo data.
+	@return multi_terms a list of terms with multiple isntances in periodo data.
+	This method populates the multi_terms global list, which is hugely important to 
+	pinpointing which date range to use based on a document's publication year.
+	"""
+
+	multi_terms = []
+	freqs = {}
+	for term in terms:
+		if term in freqs:
+			freqs[term] += 1
+		else:
+			freqs[term] = 1
+	for term in freqs:
+		if freqs[term] > 1:
+			multi_terms.append(term)
+	return multi_terms
 
 
 def find_terms(line, line_num, found_list):
@@ -156,32 +127,41 @@ def find_terms(line, line_num, found_list):
 	@param line is the string of text which to parse
 	@param line_num is the line number, used for output and checking
 		   relationship between term location and site location
-	@param human_readable is a boolean used for output
 	@param found_list is a list of lists with entries of the form
 		   [term, line_num] (must remain mutable)
 	Checks a line of text against the defined vocabularies.
 	Grabs everything relevant, nondiscretely.
 	"""
-	#TODO: If we find a term, compare pub date with periodo[8]
-	#line = re.sub(r"\s+", ' ', line)
-	terms = set_of_vals
-	for term in terms:
+
+	for term in periodo[1]:
+		print(term)
 		if term.casefold() in line:
 			for negator in NEGATIVES:
 				if negator.casefold() in line:
 					return
+			print(line)
 			line = line.replace(term.casefold(), '')
 			found_list.append([term, line_num])
 
 
 def find_times(line, line_num, found_list):
+	"""
+	@param line is the string of text which to parse
+	@param line_num is the line number, used for output and checking
+		   relationship between date location and site location
+	@param found_list is a list of lists with entries of the form
+		   [term, line_num] (must remain mutable)
+	Checks a line of text against some regex.
+	Grabs everything relevant, nondiscretely.
+	"""
 
-	#Look for BP dates (tend to get lost) w regex
+	#Look for BP dates
 	bp_check = bp_dates.findall(line)
 	if bp_check is not None:
 			for item in bp_check:
 				found_list.append(item[0])
 
+	#Look for centuries
 	century_check = cents.findall(line)
 	if century_check is not None:
 			for item in century_check:
@@ -189,12 +169,19 @@ def find_times(line, line_num, found_list):
 
 
 def parse_content(content):
-	#TODO: Docstring
+	"""
+	@param content is the content of the document to parse.
+	@return records, a set of record data structs representing site/term
+			pairings.
+	Heavy lifting method. Checks each line for trinomials, and then looks for
+	nearby terms that might be related. Builds record datastruct along the way
+	to store data for each site/term pairing.
+	"""
 
 	freqs = {}
 	records = set()
 	for line_num in range(len(content)):
-		tris_in_line = trinomial_regex.findall(content[line_num])
+		tris_in_line = TRINOMIAL_REGEX.findall(content[line_num])
 		if tris_in_line is None:
 			continue
 		for trinomial in tris_in_line:
@@ -234,6 +221,13 @@ def parse_content(content):
 
 
 def get_date(content):
+	"""
+	@param content is the text of the document.
+	Checks the first 20 lines of text, as well as the document name,
+	to try and guess what the date the document was published in.
+	If nothing can be found, just return the current year.
+	"""
+
 	now = datetime.datetime.now()
 	dates = pub_date.findall(input_file)
 	if dates:
@@ -271,7 +265,7 @@ def display_hr(records):
 		print("SITE:", record.site_name, "line", record.site_name_line)
 		print("PERIOD:", record.period_term)
 		if record.dates:
-			print("EXPLICIT DATES:", record.dates)
+			print("EXPLICIT dateS:", record.dates)
 		print("\n")
 
 
@@ -316,19 +310,19 @@ def get_search_space(content, rec):
 			search_space += content[line]
 			if rec.site_name in content[line]:
 				flag = True
+			"""
 			if content[line].strip() == "":
 				if not flag:
 					search_space = ""
 				else:
 					break
+			"""
 
 	search_space = re.sub(r"\s+", ' ', search_space)
-	local_trin_count = len(set(trinomial_regex.findall(search_space)))
-	#TODO: For some reason this fails, so use tokenize
-	#sentences = split_into_sentences(search_space)
+	local_trin_count = len(set(TRINOMIAL_REGEX.findall(search_space)))
 	sentences = tokenize.sent_tokenize(search_space)
 
-	#Parsing regex fails for image captioning, workaround
+	#Parsing fails for image captioning, workaround
 	if not sentences:
 		sentences.append(search_space)
 
@@ -337,20 +331,19 @@ def get_search_space(content, rec):
 
 def write_record(f, r):
 	"""
-	@param f a file to write to
-	@param t a Record dataclass
+	@param f, a file to write to.
+	@param t, a Record dataclass.
 	Writes the file in CSV form.
 	"""
 	f.write(r.site_name)
 	f.write("," + r.period_term)
 	index = periodo[1].index(r.period_term)
 	late_start, early_end, early_start, late_end = fix_dates(r, index)
-	print(late_start, late_end, early_start, early_end)
 	f.write("," + str(early_start))
 	f.write("," + str(late_start))
 	f.write("," + str(early_end))
 	f.write("," + str(late_end))
-	f.write("," + str(periodo[0][index])) #Write periodo[0], URI
+	f.write("," + str(periodo[0][index])) #Write URI
 	f.write("," + str(r.freq))
 	f.write("," + input_file) #Write filename
 	f.write("\n")
@@ -365,6 +358,7 @@ def fix_dates(r, index):
 	choose the one with the most recent publication date before the date
 	of publication from the source document.
 	"""
+	multi_terms = get_multi_terms(periodo[1])	
 	only_two = False
 	if r.period_term not in multi_terms:
 		if only_two and not (periodo[10][index] and periodo[11][index]):
@@ -387,7 +381,6 @@ def fix_dates(r, index):
 	return periodo[4][best_index], periodo[5][best_index], \
 		periodo[10][best_index], periodo[11][best_index]
 	
-
 
 def get_optimal_term(matches, key_index, sentences, trin):
 	"""
@@ -474,102 +467,16 @@ def distance(s, w1, w2):
 	return len(spaces)
 
 
-#iso8601Date: YYYY-MM-DDTHH:MM:SS
-def nlpGetEntities(text, host='localhost', iso8601Date='', labelLst=['DATE', 'LOCATION']):
-	"""
-	Interfaces with Stanford's NER platform
-	via socket.
-	@param labelLst accounts for term types
-	"""
-
-	labelLst = set(labelLst)
-	iso8601Date = iso8601Date.strip()
-	if len(iso8601Date) != 0:
-		iso8601Date = ',"date":"' + iso8601Date + '"'
-
-	request = host + ':9000/?properties={"annotators":"entitymentions","outputFormat":"json"' + iso8601Date + '}'
-	entities = []
-	dedupSet = set()
-
-	try:
-		output = check_output(['wget', '-q', '-O', '-', '--post-data', text, request])
-		parsed = json.loads(output.decode('utf-8'))
-
-		if 'sentences' not in parsed:
-			return []
-
-		for sent in parsed['sentences']:
-
-			if 'entitymentions' not in sent:
-				continue
-
-			for entity in sent['entitymentions']:
-
-				#text is entity, ner is entity class
-				dedupKey = entity['text'] + entity['ner']
-
-				if dedupKey in dedupSet or entity['ner'] not in labelLst:
-					continue
-
-				if len(entity['text']) != 0:
-					entities.append([entity['text'], entity['ner']])
-					dedupSet.add(dedupKey)
-	except:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		errorMessage = fname + ', ' + str(exc_tb.tb_lineno)  + ', ' + str(sys.exc_info())
-		print('\tERROR:', errorMessage)
-
-	return entities
-
-
-#Credit to D Greenberg on Github
-def split_into_sentences(text):
-	"""
-	Powerful regex that splits a text into strings.
-	"""
-
-	text = " " + text + "  "
-	text = text.replace("\n"," ")
-	text = re.sub(prefixes,"\\1<prd>",text)
-	text = re.sub(websites,"<prd>\\1",text)
-	if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-	text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-	text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-	text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-	text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-	text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-	text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-	text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-	if "”" in text: text = text.replace(".”","”.")
-	if "\"" in text: text = text.replace(".\"","\".")
-	if "!" in text: text = text.replace("!\"","\"!")
-	if "?" in text: text = text.replace("?\"","\"?")
-	text = text.replace(".",".<stop>")
-	text = text.replace("?","?<stop>")
-	text = text.replace("!","!<stop>")
-	text = text.replace("<prd>",".")
-	sentences = text.split("<stop>")
-	sentences = sentences[:-1]
-	sentences = [s.strip() for s in sentences]
-	return sentences
-
-
 def main():
-	"""
-	TODO: Docstring
-	"""
+
 	global human_readable
 	global relevant_trinomials
-	global counties
 	global periodo
-	global set_of_vals
 	global input_file
 	global date
 
 	input_file = str(sys.argv[1])
 	human_readable_input = str(sys.argv[2]).lower()
-	#human_readable = False
 	if human_readable_input == 'y' or human_readable_input == 'yes':
 		human_readable = True
 
@@ -583,12 +490,8 @@ def main():
 	l = []
 	for line in content:
 		find_times(line, 0, l)
-	print("Found in this document:")
-	print(l)
-	print("\n")
 
-	relevant_trinomials, counties, periodo = harvest()
-	set_of_vals = periodo[1]
+	relevant_trinomials, periodo = harvest()
 	records = parse_content(content)
 
 	o = open("out.csv", "a+")
@@ -600,48 +503,3 @@ def main():
 		display_hr(records)
 
 main()
-
-
-"""
-Here be monsters
-"""
-
-
-def find_more_stuff(line, line_num, human_readable, found_list):
-	#Looks for county codes and ensures we count them as counties (ha)
-	cc = countycodes.findall(line)
-	if cc is not None:
-		for item in cc:
-			if item in ccodes:
-				if(human_readable):
-					print("LOCATION " + str(cmap[item]) + " County, line " + str(line_num))
-				found_list[1].append(cmap[item])
-
-	#Look for BP dates (tend to get lost) w regex
-	bp_check = bp_dates.findall(line)
-	if bp_check is not None:
-			for item in bp_check:
-				if(human_readable):
-					print("DATE " + str(item.upper()) + ", line " + str(line_num))
-				found_list[1].append(item)
-	#Stanford NER Method, goes in and finds dates and spelled out
-	#counties
-	ents = nlpGetEntities(line)
-	if len(ents) is not 0:
-		for item in ents:
-			#Grab date
-			if item[1] == 'DATE' and item[0].lower() not in timeExclude:
-				if(human_readable):
-					print(str(item[1].upper()) +" "+ str(item[0].upper()) + ", line " + str(line_num))
-				found_list[1].append(item[0])
-			#Grab county
-			if item[1] == 'LOCATION':
-				if item[0] in cnames:
-					if(human_readable):
-						print(str(item[1].upper()) +" "+ str(item[0].upper()) + ", line " + str(line_num))
-					found_list[1].append(item[0])
-				elif ' ' in item[0] and item[0].split(' ',1)[1].upper() == 'COUNTY':
-					if item[0].split(' ',1)[0] in cnames:
-						if(human_readable):
-							print(str(item[1].upper()) +" "+ str(item[0].upper()) + ", line " + str(lineNum))
-						found_list[1].append(item[0])
